59e0fb4aa060715eea5bdf986c8db6837b28cd41
George Kapoya <george.kapoya@fi.abb.com>
2023-02-02 19:34:51 +0200

    initial commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by George Kapoya on 2.2.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
